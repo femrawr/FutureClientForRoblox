20eac857ff3373cf39cc787e86fd7f99a0396ce4
femrawr <212382825+femrawr@users.noreply.github.com>
2026-06-23 15:36:09 +0800

    Update build.bat

--- proxy/build.bat
+++ proxy/build.bat
@@ -1,1 +1,1 @@
-go build main.go -trimpath -buildvcs=false -ldflags="-s -w -H windowsgui -buildid=" -o proxy.exe
+go build -trimpath -buildvcs=false -ldflags="-s -w -H windowsgui -buildid=" -o proxy.exe main.go
